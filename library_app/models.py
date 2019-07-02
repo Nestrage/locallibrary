ba55c9d572456becf2af206623b23f2328b7d780
Nestrage <dimakoujav@gmail.com>
2019-07-02 15:15:52 +0300

    initial commit

--- library_app/models.py
+++ library_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.utils import timezone
-
+#qweqe
 
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
